ccf778dafb4193efc1391d1907bc35cd5950a3ac
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2023-08-02 13:35:51 +0200

    Add test and add dependency on upload_field_improvements module

---
+++ tests/src/Unit/ValueObjects/UploadDimensionsTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\tengstrom_configuration\ValueObjects;
+
+use Drupal\tengstrom_configuration\ValueObjects\UploadDimensions;
+use Drupal\Tests\UnitTestCase;
+
+class UploadDimensionsTest extends UnitTestCase {
+
+  /**
+   * @test
+   *
+   * phpcs:disable Drupal.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
+   */
+  public function fromArray_throws_exception_on_missing_width(): void {
+    $values = ['height' => 20];
+
+    $this->expectExceptionMessage('Width is required in upload dimensions');
+    UploadDimensions::fromArray($values);
+  }
+
+  /**
+   * @test
+   *
+   * phpcs:disable Drupal.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
+   */
+  public function fromArray_throws_exception_on_missing_height(): void {
+    $values = ['width' => 20];
+
+    $this->expectExceptionMessage('Height is required in upload dimensions');
+    UploadDimensions::fromArray($values);
+  }
+
+  /**
+   * @test
+   *
+   * phpcs:disable Drupal.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
+   */
+  public function fromArray_creates_object_for_valid_input(): void {
+    $values = ['width' => 20, 'height' => 40];
+
+    $dimensions = UploadDimensions::fromArray($values);
+    $this->assertSame($values['width'], $dimensions->getWidth());
+    $this->assertSame($values['height'], $dimensions->getHeight());
+  }
+
+}
